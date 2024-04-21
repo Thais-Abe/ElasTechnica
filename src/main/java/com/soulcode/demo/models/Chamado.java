0317e1bbf6b75f216f0629f0f5eb02c6d3ea1458
Jessica Leite <jessica.mleite@gmail.com>
2024-04-21 15:36:39 -0300

    Anotacoes Entidades correcoes

--- src/main/java/com/soulcode/demo/models/Chamado.java
+++ src/main/java/com/soulcode/demo/models/Chamado.java
@@ -1,21 +1,31 @@
 package com.soulcode.demo.models;
-//import jakarta.persistence.*;
 
+import jakarta.persistence.*;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
-//@Entity
+@Entity
+@Table(name = "Chamado")
 public class Chamado {
-    //@Id
-    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int Id;
     private String Titulo;
     private String Descricao;
     private int Prioridade;
-    private Date DataInicio;
+    private LocalDateTime DataInicio;
+    @ManyToOne
+    @JoinColumn(name = "setor_id")
     private Setor Setor;
+    @ManyToOne
+    @JoinColumn(name = "status_id")
     private Status Status;
+    @ManyToOne
+    @JoinColumn(name = "tecnico_id")
     private Pessoa Tecnico;
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
     private Pessoa Usuario;
 
 }
