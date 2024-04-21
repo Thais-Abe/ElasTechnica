28090273aee84fdb148a9c5f6b3c4cb7340a6b4a
Jessica Leite <jessica.mleite@gmail.com>
2024-04-21 14:47:31 -0300

    Anotacoes Entidades

--- src/main/java/com/soulcode/demo/models/Pessoa.java
+++ src/main/java/com/soulcode/demo/models/Pessoa.java
@@ -1,9 +1,23 @@
 package com.soulcode.demo.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
+
+@Entity
 public class Pessoa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int Id;
+
+    @Column
     private String Nome;
+
+    @Column
     private String Email;
+
+    @Column
     private String Senha;
     private Setor Setor;
     private Tipo Tipo;
--- src/main/java/com/soulcode/demo/models/Tipo.java
+++ src/main/java/com/soulcode/demo/models/Tipo.java
@@ -1,6 +1,13 @@
 package com.soulcode.demo.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+
+@Entity
 public class Tipo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int Id;
+    @Column
     private String Nome;
 }
