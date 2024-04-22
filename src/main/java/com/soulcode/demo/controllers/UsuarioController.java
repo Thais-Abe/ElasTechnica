fd6d2753ad5bdd1c47f06dbf0eeb197bacdf4ebd
Sabrina <sabrinaslimap@gmail.com>
2024-04-21 22:35:40 -0300

    Adicionando lógica de login

--- src/main/java/com/soulcode/demo/controllers/UsuarioController.java
+++ src/main/java/com/soulcode/demo/controllers/UsuarioController.java
@@ -12,9 +12,31 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class UsuarioController {
 
+    @Autowired
+    PessoaRepository pessoaRepository;
+
     @GetMapping("/login-usuario")
     public String paginaLoginUsuario() {
         return "login-usuario";
+    }
+
+    @RequestMapping(value = "/login-usuario", method = RequestMethod.POST)
+    public String login(@RequestParam String email, @RequestParam String senha, Model model) {
+        Pessoa usuario = pessoaRepository.findByEmail(email);
+
+        if (usuario != null && usuario.getSenha().equals(senha)) {
+            int tipoUsuario = usuario.getTipo().getId();
+
+            if (tipoUsuario == 1) {
+                return "redirect:/pagina-cliente";
+            } else {
+                return "redirect:/pagina-tecnico";
+            }
+
+        } else {
+            model.addAttribute("error", "E-mail ou senha incorretos");
+            return "login-usuario";
+        }
     }
 
     @GetMapping("/pagina-usuario")
@@ -28,44 +50,38 @@ public class UsuarioController {
         return "abertura-chamado";
     }
 
-
     @GetMapping("/detalhes-chamado")
     public String teste5(@RequestParam("nome") String nome, Model model, @RequestParam("setor") String setor, @RequestParam("prioridade") String prioridade, @RequestParam("solicitacao") String solicitacao) {
 
         model.addAttribute("nome", nome);
         model.addAttribute("setor", setor);
-//        model.addAttribute("data", LocalDate.now());
+//      model.addAttribute("data", LocalDate.now());
         model.addAttribute("prioridade", prioridade);
         model.addAttribute("solicitacao", solicitacao);
         return "detalhes-chamado";
     }
 
-    @Autowired
-    PessoaRepository pessoaRepository;
-
     @GetMapping("/cadastro-usuario")
     public String criarUsuario() {
-
         return "cadastro-usuario";
     }
 
     @RequestMapping(value = "/cadastro-usuario", method = RequestMethod.POST)
     public String save(@RequestParam String nome , @RequestParam String email, String senha, int tipoId, int setorId, Model model) {
-
         Pessoa usuario = new Pessoa();
         usuario.setNome(nome);
         usuario.setEmail(email);
         usuario.setSenha(senha);
+
         Tipo tipo = new Tipo();
         tipo.setId(tipoId);
         usuario.setTipo(tipo);
+
         Setor setor = new Setor();
         setor.setId(setorId);
         usuario.setSetor(setor);
+
         pessoaRepository.save(usuario);
         return "redirect:/login-usuario";
-
     }
-
-
 }
--- src/main/java/com/soulcode/demo/repositories/PessoaRepository.java
+++ src/main/java/com/soulcode/demo/repositories/PessoaRepository.java
@@ -2,7 +2,10 @@ package com.soulcode.demo.repositories;
 
 import com.soulcode.demo.models.Pessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Integer> {
 
+        @Query("SELECT p FROM Pessoa p WHERE p.Email = :email")
+        Pessoa findByEmail(String email);
 }
