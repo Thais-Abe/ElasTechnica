4c6c39e092bcbc728f86985d0728cfb36e865f68
Sabrina <sabrinaslimap@gmail.com>
2024-04-24 08:08:57 -0300

    Adicionando a mudança de status ao chamado

--- src/main/java/com/soulcode/demo/controllers/TecnicoController.java
+++ src/main/java/com/soulcode/demo/controllers/TecnicoController.java
@@ -1,18 +1,17 @@
 package com.soulcode.demo.controllers;
 
 import com.soulcode.demo.models.Chamado;
+import com.soulcode.demo.models.Pessoa;
 import com.soulcode.demo.models.Status;
 import com.soulcode.demo.repositories.ChamadoRepository;
 import com.soulcode.demo.repositories.StatusRepository;
 import com.soulcode.demo.services.ChamadoService;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,13 +31,7 @@ public class TecnicoController {
     private static boolean chamadosForamRegistrados = false;
 
     @GetMapping("/pagina-tecnico")
-    public String paginaTecnico(Model model, HttpServletRequest request, @RequestParam(required = false) String status, @RequestParam(required = false) String nome) {
-        if ("atualizado".equals(status)) {
-            return "redirect:/pagina-tecnico";
-        }
-
-        model.addAttribute("nome", nome);
-
+    public String paginaTecnico(@RequestParam(required = false) String nome, Model model, HttpServletRequest request) {
         if (!chamadosForamRegistrados) {
             chamadoService.registrarChamadosFicticios(request);
             chamadosForamRegistrados = true;
@@ -51,7 +44,7 @@ public class TecnicoController {
         for (Chamado chamado : todosChamados) {
             if (chamado.getStatus().getId() == 1) {
                 chamadosDisponiveis.add(chamado);
-            } else if (chamado.getStatus().getId() == 2 || chamado.getStatus().getId() == 4) {
+            } else if (chamado.getStatus().getId() == 2 || chamado.getStatus().getId() == 3 || chamado.getStatus().getId() == 4) {
                 chamadosEmAtendimento.add(chamado);
             }
         }
@@ -65,10 +58,10 @@ public class TecnicoController {
 
         model.addAttribute("chamadosDisponiveis", chamadosDisponiveis);
         model.addAttribute("chamadosEmAtendimento", chamadosEmAtendimento);
+        model.addAttribute("nome", nome);
 
         return "pagina-tecnico";
     }
-
 
     @GetMapping("/detalhes-chamado/{id}")
     public String detalhesChamado(@PathVariable("id") int id, Model model) {
@@ -80,29 +73,32 @@ public class TecnicoController {
         return "detalhes-chamado";
     }
 
-    @PostMapping("/mudar-status")
-    public String mudarStatusChamado(@RequestParam int id, @RequestParam int status) {
+    @RequestMapping(value = "/mudar-status", method = RequestMethod.POST)
+    public String mudarStatusChamado(@RequestParam(required = false) String nome, @RequestParam int id, @RequestParam int status, HttpServletRequest request, HttpSession session) {
+
         Chamado chamado = chamadoService.obterChamadoPorId(id);
+        Pessoa tecnicoLogado = (Pessoa) session.getAttribute("usuarioLogado");
 
-        if (status == 1) {
-            Status novoStatus = statusRepository.findById(2).orElse(null);
-            if (novoStatus != null) {
-                chamado.setStatus(novoStatus);
-                chamadoService.salvarChamado(chamado);
+        Status statusAtualizado = null;
 
-                return "redirect:/pagina-tecnico?status=atualizado";
-            }
-        } else if (status == 2) {
-            Status novoStatus = statusRepository.findById(4).orElse(null);
-            if (novoStatus != null) {
-                chamado.setStatus(novoStatus);
-                chamadoService.salvarChamado(chamado);
-
-                return "redirect:/pagina-tecnico?status=atualizado";
-            }
+        switch (status) {
+            case 1 -> statusAtualizado = statusRepository.findById(2).orElse(null); // na base de dados o id 2 = Em atendimento
+            case 2 -> statusAtualizado = statusRepository.findById(3).orElse(null); // na base de dados o id 3 = Escalado para outro setor
+            case 3 -> statusAtualizado = statusRepository.findById(4).orElse(null); // na base de dados o id 4 = Finalizado
+            default -> { return "redirect:/pagina-tecnico";}
         }
 
-        return "redirect:/pagina-tecnico";
+        if (statusAtualizado != null) {
+            chamado.setStatus(statusAtualizado);
+            chamado.setTecnico(tecnicoLogado);
+            chamadoService.salvarChamado(chamado);
+
+            session.setAttribute("tecnicoLogadoNome", tecnicoLogado.getNome());
+
+            return "redirect:/pagina-tecnico?nome=" + tecnicoLogado.getNome();
+        } else {
+            return "redirect:/pagina-tecnico";
+        }
     }
 
     public List<Chamado> getChamadosFicticiosDoBancoDeDados() {
